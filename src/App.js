8155fa76fde1f363bf5f36187f1e812ccd70aa53
Nestor Mamani <nmamanipantoja@gmail.com>
2021-11-07 17:51:08 -0500

    🧱 Building the GIF grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import twitterLogo from './assets/twitter-logo.svg';
+import marvelLogo from './assets/marvel.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -8,9 +11,110 @@ const ARTISAN_HANDLE = 'nestoredduardo';
 const ARTISAN_LINK = `https://twitter.com/${ARTISAN_HANDLE}`;
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [gifList, setGifList] = useState([]);
+  const TEST_GIFS = [
+    'https://media.giphy.com/media/vBjLa5DQwwxbi/giphy.gif',
+    'https://media.giphy.com/media/6ra84Uso2hoir3YCgb/giphy.gif',
+    'https://media.giphy.com/media/h0gzUb0Wh1RIY/giphy.gif',
+    'https://media.giphy.com/media/93RImebAuEKkgRzcPr/giphy.gif',
+  ];
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+          );
+
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <input
+        type="text"
+        placeholder="Enter gif link!"
+        value={inputValue}
+        onChange={onInputchange}
+      />
+      <button onClick={sendGif} className="cta-button submit-gif-button">
+        Submit
+      </button>
+      <div className="gif-grid">
+        {gifList.map((gif) => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
+  useEffect(() => {
+    window.addEventListener('load', async (event) => {
+      await checkIfWalletIsConnected();
+    });
+  }, []);
+
+  useEffect(() => {
+    if (walletAddress) {
+      console.log('Fetching GIF list...');
+
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
+
+  const onInputchange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log('Gif link:', inputValue);
+    } else {
+      console.log('Empty Input. Try Again.');
+    }
+  };
+
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
           <iframe
             src="https://giphy.com/embed/t6lGyf3dk9qBEXhTj8"
@@ -20,10 +124,14 @@ const App = () => {
             class="giphy-embed"
             allowFullScreen
           ></iframe>
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">
+            <img src={marvelLogo} className="marvel-logo" /> Marvel GIF Portal
+          </p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
+          {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
