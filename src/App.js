3e07cb5dd750f5cf5eb457673f2bc84bbea3e423
Nestor Mamani <nmamanipantoja@gmail.com>
2021-11-07 13:21:37 -0500

    Personalized Home

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import './App.css';
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const ARTISAN_HANDLE = 'nestoredduardo';
+const ARTISAN_LINK = `https://twitter.com/${ARTISAN_HANDLE}`;
 
 const App = () => {
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
+          <iframe
+            src="https://giphy.com/embed/t6lGyf3dk9qBEXhTj8"
+            width="300"
+            height="300"
+            frameBorder="0"
+            class="giphy-embed"
+            allowFullScreen
+          ></iframe>
           <p className="header">🖼 GIF Portal</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
@@ -23,6 +33,12 @@ const App = () => {
             target="_blank"
             rel="noreferrer"
           >{`built on @${TWITTER_HANDLE}`}</a>
+          <a
+            className="footer-text css-e657no"
+            href={ARTISAN_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`by @${ARTISAN_HANDLE}`}</a>
         </div>
       </div>
     </div>
